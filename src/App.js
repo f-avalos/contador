f80a089f95b005a790989c1d4bd6c6fd58b38d8b
Jorge <jorgexm99@gmail.com>
2022-05-14 19:37:44 -0400

    Termminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './componentes/Button'
+import Contador from './componentes/Contador'
+import freecodecampLogo from './img/freecodecamp-logo.png'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	//usestate para estados
+	//estado en 0
+	const [numClics, setNumClics] = useState(0)
+
+	const manejarClick = () => {
+		//Aumentar estado en 1
+		setNumClics(numClics + 1)
+	}
+
+	const reiniciarContador = () => {
+		//reinciar estado en 0
+		setNumClics(0)
+	}
+
+	return (
+		//Aplicación principal
+		<div className="App">
+			{/* Logo */}
+			<div className='freecodecamp-logo-contenedor'>
+				<img 
+				className='freecodecamp-logo'
+				src={freecodecampLogo}
+				alt='Logo de freeCodeCamp' />
+			</div>
+			{/* Contador y botones */}
+			<div className='contenedor-principal'>
+				<Contador 
+				numClics={numClics} />
+				{/* 2 botones con parámetros a enviar a /componentes */}
+				<Button 
+				texto='Click!' //texto dentro de botón
+				esBotonDeClick={true} //boolean para asignar clases según tipo de botón
+				manejarClick={manejarClick} /> {/* Función para gestionar clics */}
+				<Button
+				texto='Reiniciar'
+				esBotonDeClick={false}
+				manejarClick={reiniciarContador} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
